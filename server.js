608180dcc3202063960f24d274a80a626c86a02b
nicknamejv <jonvercabral@yahoo.com>
2021-09-07 16:45:01 -0700

    created initial server.js setup and index route works

---
+++ server.js
@@ -0,0 +1,34 @@
+// SECTION: External Modules 
+const { response } = require('express');
+const express = require('express');
+const app = express();
+const methodOverride = require('method-override');
+const PORT = 4000;
+
+
+// SECTION: Internal Modules
+
+
+
+// SECTION: App Config
+app.set('view engine', 'ejs');
+
+
+// SECTION: Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+
+// SECTION: Controller Files
+
+
+// SECTION: Index Page
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    // res.redirect('/journeyjapan');
+});
+
+app.listen(PORT, () => {
+    console.log(`I live on port ${PORT}`);
+});
