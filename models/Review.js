1b3e837f7d95e6bec79641d152a58957eb4dca1d
nicknamejv <jonvercabral@yahoo.com>
2021-09-07 20:55:57 -0700

    Created user, city, thingstodo, and review models

---
+++ models/Review.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+    {
+        rating: {
+            type: Number,
+            required: true,
+        },
+
+        content: {
+            type: String,
+            required: true,
+        },
+
+        thingstodo: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'ThingsToDo',
+            required: true,
+        },
+
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+    },
+
+    {
+        timestamps: true,
+    },
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
---
+++ models/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    City: require('./City'),
+    Review: require('./Review'),
+    ThingsToDo: require('./ThingsToDo'),
+    User: require('./User'),
+};
