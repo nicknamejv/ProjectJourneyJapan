c0b1fe16a30e4b554c64665587d6a670054c7e86
nicknamejv <jonvercabral@yahoo.com>
2021-09-12 19:10:07 -0700

    adjusting footer and navbar

--- controllers/city_controller.js
+++ controllers/city_controller.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { City, ThingsToDo } = require("../models");
 
 // NOTE: Show Route
-router.get("/:id", async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
     try {
         const allCities = await City.find({});
         const foundCity = await City.findById(req.params.id);
